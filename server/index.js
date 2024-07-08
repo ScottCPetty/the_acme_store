a4fe58ee9378e7f9d00e711a177d5bb82901c1fe
ScottCPetty <scottishcheese13@gmail.com>
2024-07-08 12:00:44 -0400

    package.json change

--- server/index.js
+++ server/index.js
@@ -13,9 +13,27 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-api.get("/api/users", async (req, res, next) => {});
-api.get("/api/products", async (req, res, next) => {});
-api.get("/api/users/:id/favorites", async (req, res, next) => {});
+api.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (error) {
+    next(error);
+  }
+});
+api.get("/api/products", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (error) {
+    next(error);
+  }
+});
+api.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    res.send(await fetchFavorites(req.params.id));
+  } catch (error) {
+    next(error);
+  }
+});
 api.post("/api/users/:id/favorites", async (req, res, next) => {});
 api.delete("/api/users/:userId/favorites/:id", async (req, res, next) => {});
 
@@ -24,5 +42,7 @@ const init = async () => {
   console.log("connected to database");
   await createTables();
   console.log("created tables");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 init();
