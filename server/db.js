015bbe9cec2c921a8e8ef7941df1069209190c22
ScottCPetty <scottishcheese13@gmail.com>
2024-07-08 11:06:39 -0400

    boilerplate

---
+++ server/db.js
@@ -0,0 +1,6 @@
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
+);
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
